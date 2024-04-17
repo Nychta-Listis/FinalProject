461123a4fb0f6e191caf27c1ef8135ddb5d25d93
alex <alexandern@andrews.edu>
2024-04-16 23:50:41 -0400

    First Database setup

---
+++ app/src/main/java/edu/andrews/edu/cptr252/alexandern/finalproject/DAOEvents.java
@@ -0,0 +1,190 @@
+package edu.andrews.edu.cptr252.alexandern.finalproject;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.media.metrics.Event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DAOEvents  extends SQLiteOpenHelper {
+
+    private static final int VERSION = 1;
+    private final String TABLE = "events";
+    private static final String DATABASE = "EventList";
+
+    public DAOEvents(Context context) {
+        super(context, DATABASE, null, VERSION);
+    }
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE "+ TABLE +
+                " (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                " eventId TEXT NOT NULL, " +
+                " name TEXT, " +
+                " text TEXT, " +
+                " choice1txt TEXT, " +
+                " choice1id TEXT, " +
+                " choice2txt TEXT, " +
+                " choice2id TEXT, " +
+                " isInitial TEXT NOT NULL);";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
+
+    public List<EventData> getFullList(String order){
+        List<EventData> events = new ArrayList<>();
+
+        Cursor cursor = getReadableDatabase().rawQuery("SELECT * FROM "+TABLE+
+                " ORDER BY eventId "+order+";", null);
+
+        EventData Menu = new EventData();
+        Menu.setName("Main Menu");
+        Menu.setId("mainMenu");
+        Menu.setIdDB(-2L);
+        events.add(Menu);
+
+        EventData emptyEvent = new EventData();
+        emptyEvent.setName("emptyEvent");
+        emptyEvent.setId("emptyEvent");
+        emptyEvent.setIdDB(-3L);
+        events.add(emptyEvent);
+
+        while(cursor.moveToNext()){
+            EventData e = new EventData();
+
+            int IdIndex = cursor.getColumnIndex("id");
+            int eventIdIndex = cursor.getColumnIndex("eventId");
+            int nameIndex = cursor.getColumnIndex("name");
+            int textIndex = cursor.getColumnIndex("text");
+            int choice1txtIndex = cursor.getColumnIndex("choice1txt");
+            int choice1idIndex = cursor.getColumnIndex("choice1id");
+            int choice2txtIndex = cursor.getColumnIndex("choice2txt");
+            int choice2idIndex = cursor.getColumnIndex("choice2id");
+            int isInitialIndex = cursor.getColumnIndex("isInitial");
+
+            e.setIdDB(cursor.getLong(IdIndex));
+            e.setId(cursor.getString(eventIdIndex));
+            e.setName(cursor.getString(nameIndex));
+            e.setText(cursor.getString(textIndex));
+            e.setChoice1txt(cursor.getString(choice1txtIndex));
+            e.setChoice1id(cursor.getString(choice1idIndex));
+            e.setChoice2txt(cursor.getString(choice2txtIndex));
+            e.setChoice2id(cursor.getString(choice2idIndex));
+            e.setIsInitial(cursor.getString(isInitialIndex));
+
+            events.add(e);
+        }
+        cursor.close();
+        return events;
+    }
+
+    public List<EventData> getEditList(String order){
+        List<EventData> events = new ArrayList<>();
+
+        Cursor cursor = getReadableDatabase().rawQuery("SELECT * FROM "+TABLE+
+                " ORDER BY eventId "+order+";", null);
+        while(cursor.moveToNext()){
+            EventData e = new EventData();
+
+            int IdIndex = cursor.getColumnIndex("id");
+            int eventIdIndex = cursor.getColumnIndex("eventId");
+            int nameIndex = cursor.getColumnIndex("name");
+            int textIndex = cursor.getColumnIndex("text");
+            int choice1txtIndex = cursor.getColumnIndex("choice1txt");
+            int choice1idIndex = cursor.getColumnIndex("choice1id");
+            int choice2txtIndex = cursor.getColumnIndex("choice2txt");
+            int choice2idIndex = cursor.getColumnIndex("choice2id");
+            int isInitialIndex = cursor.getColumnIndex("isInitial");
+
+            e.setIdDB(cursor.getLong(IdIndex));
+            e.setId(cursor.getString(eventIdIndex));
+            e.setName(cursor.getString(nameIndex));
+            e.setText(cursor.getString(textIndex));
+            e.setChoice1txt(cursor.getString(choice1txtIndex));
+            e.setChoice1id(cursor.getString(choice1idIndex));
+            e.setChoice2txt(cursor.getString(choice2txtIndex));
+            e.setChoice2id(cursor.getString(choice2idIndex));
+            e.setIsInitial(cursor.getString(isInitialIndex));
+
+            events.add(e);
+        }
+        cursor.close();
+        return events;
+    }
+
+    public List<EventData> getInitialList(String order){
+        List<EventData> events = new ArrayList<>();
+
+        Cursor cursor = getReadableDatabase().rawQuery("SELECT * FROM "+TABLE+
+                " ORDER BY eventId "+order+";", null);
+        while(cursor.moveToNext()){
+            EventData e = new EventData();
+
+            int IdIndex = cursor.getColumnIndex("id");
+            int eventIdIndex = cursor.getColumnIndex("eventId");
+            int nameIndex = cursor.getColumnIndex("name");
+            int textIndex = cursor.getColumnIndex("text");
+            int choice1txtIndex = cursor.getColumnIndex("choice1txt");
+            int choice1idIndex = cursor.getColumnIndex("choice1id");
+            int choice2txtIndex = cursor.getColumnIndex("choice2txt");
+            int choice2idIndex = cursor.getColumnIndex("choice2id");
+            int isInitialIndex = cursor.getColumnIndex("isInitial");
+
+            e.setIdDB(cursor.getLong(IdIndex));
+            e.setId(cursor.getString(eventIdIndex));
+            e.setName(cursor.getString(nameIndex));
+            e.setText(cursor.getString(textIndex));
+            e.setChoice1txt(cursor.getString(choice1txtIndex));
+            e.setChoice1id(cursor.getString(choice1idIndex));
+            e.setChoice2txt(cursor.getString(choice2txtIndex));
+            e.setChoice2id(cursor.getString(choice2idIndex));
+            e.setIsInitial(cursor.getString(isInitialIndex));
+
+            if(e.getIsInitial().equals("1")){
+                events.add(e);
+            }
+        }
+        cursor.close();
+        return events;
+    }
+
+    public void insertEvent (EventData e){
+        ContentValues values = new ContentValues();
+        values.put("eventId",e.getId());
+        values.put("name",e.getName());
+        values.put("text",e.getText());
+        values.put("choice1txt",e.getChoice1txt());
+        values.put("choice1id",e.getChoice1id());
+        values.put("choice2txt",e.getChoice2txt());
+        values.put("choice2id",e.getChoice2id());
+        values.put("isInitial",e.getIsInitial());
+        getWritableDatabase().insert(TABLE,null,values);
+    }
+
+    public void editEvent (EventData e){
+        ContentValues values = new ContentValues();
+        values.put("id",e.getIdDB());
+        values.put("eventId",e.getId());
+        values.put("name",e.getName());
+        values.put("text",e.getText());
+        values.put("choice1txt",e.getChoice1txt());
+        values.put("choice1id",e.getChoice1id());
+        values.put("choice2txt",e.getChoice2txt());
+        values.put("choice2id",e.getChoice2id());
+        values.put("isInitial",e.getIsInitial());
+
+        String[] idToEdit = {e.getIdDB().toString()};
+        getWritableDatabase().update(TABLE,values,"id=?",idToEdit);
+    }
+
+    public void deleteContact (EventData e){
+        SQLiteDatabase db = getWritableDatabase();
+        String[] idToDelete = {e.getIdDB().toString()};
+        db.delete(TABLE,"id=?",idToDelete);
+    }
+}
---
+++ app/src/main/java/edu/andrews/edu/cptr252/alexandern/finalproject/EventData.java
@@ -0,0 +1,130 @@
+package edu.andrews.edu.cptr252.alexandern.finalproject;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class EventData implements Parcelable {
+
+    private String id="";
+    private String name="";
+    private String text="";
+    private String choice1txt="";
+    private String choice1id="";
+    private String choice2txt="";
+    private String choice2id="";
+    private String isInitial="0";
+    private Long idDB = -1L;
+
+    public EventData(){}
+    private EventData(Parcel source) {
+        String[] data = new String[8];
+        source.readStringArray(data);
+        setId(data[0]);
+        setName(data[1]);
+        setText(data[2]);
+        setChoice1txt(data[3]);
+        setChoice1id(data[4]);
+        setChoice2txt(data[5]);
+        setChoice2id(data[6]);
+        setIsInitial(data[7]);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeStringArray(new String[]{
+                getId(),getName(),getText(),getChoice1txt(),getChoice1id(),getChoice2txt(),getChoice2id(),getIsInitial()
+        });
+    }
+
+    public static final Parcelable.Creator<EventData> CREATOR=new Parcelable.Creator<EventData>(){
+
+        @Override
+        public EventData createFromParcel(Parcel source) {
+            return new EventData(source);
+        }
+
+        @Override
+        public EventData[] newArray(int size) {
+            return new EventData[0];
+        }
+    };
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getChoice1txt() {
+        return choice1txt;
+    }
+
+    public void setChoice1txt(String choice1txt) {
+        this.choice1txt = choice1txt;
+    }
+
+    public String getChoice1id() {
+        return choice1id;
+    }
+
+    public void setChoice1id(String choice1id) {
+        this.choice1id = choice1id;
+    }
+
+    public String getChoice2txt() {
+        return choice2txt;
+    }
+
+    public void setChoice2txt(String choice2txt) {
+        this.choice2txt = choice2txt;
+    }
+
+    public String getChoice2id() {
+        return choice2id;
+    }
+
+    public void setChoice2id(String choice2id) {
+        this.choice2id = choice2id;
+    }
+
+    public String getIsInitial() {
+        return isInitial;
+    }
+
+    public void setIsInitial(String isInitial) {
+        this.isInitial = isInitial;
+    }
+
+    public Long getIdDB() {
+        return idDB;
+    }
+
+    public void setIdDB(Long idDB) {
+        this.idDB = idDB;
+    }
+}
